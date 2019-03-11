3dc2cfd79d8047f61c551cb0760d7934c9944075
Said Elshibiny <saidhassan.elshibiny01@stclairconnect.ca>
2019-03-11 16:50:15 -0400

    #8 added the images as buttons and set up the home page layout

--- app/src/main/java/com/saidelshibiny/badmintonplanningandroidapp/MainFragment.java
+++ app/src/main/java/com/saidelshibiny/badmintonplanningandroidapp/MainFragment.java
@@ -64,7 +64,9 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        //set the title
         getActivity().setTitle("Home Page");
+
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
